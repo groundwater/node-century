58d1d22b1f2e8c72b8994daf9d1f0fe568291590
Jesús Leganés Combarro <piranna@gmail.com>
2014-08-29 15:25:50 +0200

    Default to REPL is EXEC command is not defined

--- century.js
+++ century.js
@@ -6,55 +6,66 @@ var argv    = require('minimist')(process.argv.slice(2))
 var spawn   = require('child_process').spawn
 var binding = require('./build/Release/binding.node')
 
-var interactive = argv.interactive || false
+var interactive = argv.interactive
+var verbose     = argv.verbose || argv.v
 var ival        = argv.interval || argv.i || 3000;
 
-if (argv._.length == 0) {
-  console.log('Usage: init EXEC [ARGS...]')
+if (argv._.length == 0 && !interactive) {
+  console.log('Usage: init [ARGS...] EXEC')
 
   process.exit(1)
+}
+
+function log(){
+  if (verbose) console.log.apply(console, arguments)
 }
 
 
 // setup wait interval
 
 setInterval(function(){
-  if (interactive) console.log('calling wait')
+  log('calling wait')
 
   var status = binding.wait()
 
-  if (interactive) console.log('status', status)
+  log('status', status)
 }, ival)
 
 
-if (interactive) console.log('wait interval: %sms', ival)
+log('wait interval: %sms', ival)
 
 
 // start first runner
 
 var args = argv._
 var exec = args.shift()
-var opts = {
 
-  // the child should be attached to the same terminal
-  // as the init process, in case the user wants an
-  // immediately interactive experience
-
-  stdio: 'inherit'
+if(exec) {
+  console.log('No EXEC command found, starting REPL session')
+  require("repl").start()
 }
+else {
+  var opts = {
+    // the child should be attached to the same terminal
+    // as the init process, in case the user wants an
+    // immediately interactive experience
 
-var proc = spawn(exec, args, opts)
-
-proc.on('exit', function(code, signal){
-  if (interactive) console.log('first runner exited')
-
-  // do not exit 0 when first runner is terminated via signal
-  if (signal) code = 2
-
-  if (interactive) console.log('signal', signal ? signal : code)
-
-  // shutdown in interactive mode
-  if (interactive) {
-    process.exit(code)
+    stdio: 'inherit'
   }
-})
+
+  var proc = spawn(exec, args, opts)
+
+  proc.on('exit', function(code, signal){
+    log('first runner exited')
+
+    // do not exit 0 when first runner is terminated via signal
+    if (signal) code = 2
+
+    log('signal', signal ? signal : code)
+
+    // shutdown in interactive mode
+    if (interactive) {
+      process.exit(code)
+    }
+  })
+}
