ec268830e58768c5e1299640229565951b77c30d
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2014-10-26 22:18:33 +0100

    Use '/sbin/init' by default in non-interactive mode

--- century.js
+++ century.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+var fs = require('fs')
+
 var argv    = require('minimist')(process.argv.slice(2))
 var spawn   = require('child_process').spawn
 var binding = require('./build/Release/binding.node')
@@ -9,9 +11,68 @@ var binding = require('./build/Release/binding.node')
 var interactive = argv.interactive
 var verbose     = argv.verbose  || argv.v
 var ival        = argv.interval || argv.i || 3000;
-
 var args = argv._
 var exec = args.shift()
+
+
+function log(){
+  if (verbose) console.log.apply(console, arguments)
+}
+
+function firstRunner(exec)
+{
+  fs.stat(exec, function(error, stats)
+  {
+    if(error)
+    {
+      console.log('No EXEC command found, starting REPL session')
+
+      return require('repl').start('century> ').on('exit', function()
+      {
+        console.log('Got "exit" event from repl!');
+        process.exit(2);
+      });
+    }
+
+    // setup wait interval
+
+    setInterval(function(){
+      log('calling wait')
+
+      var status = binding.wait()
+
+      log('status', status)
+    }, ival)
+
+    log('wait interval: %sms', ival)
+
+
+    // start first runner
+
+    var opts = {
+      // the child should be attached to the same terminal
+      // as the init process, in case the user wants an
+      // immediately interactive experience
+      stdio: 'inherit'
+    }
+
+    var proc = spawn(exec, args, opts)
+
+    proc.on('exit', function(code, signal)
+    {
+      log('first runner exited')
+
+      // do not exit 0 when first runner is terminated via signal
+      if(signal) code = 3
+
+      log('signal', signal ? signal : code)
+
+      // shutdown in interactive mode
+      if(interactive) process.exit(code)
+    })
+  })
+}
+
 
 if(!exec)
 {
@@ -22,56 +83,8 @@ if(!exec)
     process.exit(1)
   }
 
-  console.log('No EXEC command found, starting REPL session')
-
-  require('repl').start('century> ').on('exit', function()
-  {
-    console.log('Got "exit" event from repl!');
-    process.exit(2);
-  });
-
-  return
+  // Exec command not defined, try to exec default at initramfs /sbin/init
+  exec = '/sbin/init';
 }
 
-
-function log(){
-  if (verbose) console.log.apply(console, arguments)
-}
-
-
-// setup wait interval
-
-setInterval(function(){
-  log('calling wait')
-
-  var status = binding.wait()
-
-  log('status', status)
-}, ival)
-
-log('wait interval: %sms', ival)
-
-
-// start first runner
-
-var opts = {
-  // the child should be attached to the same terminal
-  // as the init process, in case the user wants an
-  // immediately interactive experience
-  stdio: 'inherit'
-}
-
-var proc = spawn(exec, args, opts)
-
-proc.on('exit', function(code, signal)
-{
-  log('first runner exited')
-
-  // do not exit 0 when first runner is terminated via signal
-  if(signal) code = 3
-
-  log('signal', signal ? signal : code)
-
-  // shutdown in interactive mode
-  if(interactive) process.exit(code)
-})
+firstRunner(exec);
